7a5da252438d7d0d5df08034d38e6ece81441eb7
ivmanikhin <ivmanikhin@gmail.com>
2021-11-11 06:42:28 +0300

    something new

--- main.py
+++ main.py
@@ -8,10 +8,36 @@
 # print(result)
 from random import randint
 
-names = ['Angela', 'Jack', 'Nick', 'Sally', 'Molly', 'Steve', 'Alex']
+# names = ['Angela', 'Jack', 'Nick', 'Sally', 'Molly', 'Steve', 'Alex']
+#
+# scores = {'Angela': 48466, 'Jack': 38775, 'Nick': 96570, 'Sally': 35578, 'Molly': 94529, 'Steve': 42846, 'Alex': 31953}
+# print(scores)
+#
+# passed = {name: score for (name, score) in scores.items() if score > 90000}
+# print(passed)
 
-scores = {'Angela': 48466, 'Jack': 38775, 'Nick': 96570, 'Sally': 35578, 'Molly': 94529, 'Steve': 42846, 'Alex': 31953}
-print(scores)
+# sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
+# # Don't change code above 👆
+#
+# # Write your code below:
+# words = {word: len(word) for word in sentence.split()}
+# result = words
+# print(result)
 
-passed = {name: score for (name, score) in scores.items() if score > 90000}
-print(passed)
+weather_c = {
+    "Monday": 12,
+    "Tuesday": 14,
+    "Wednesday": 15,
+    "Thursday": 14,
+    "Friday": 21,
+    "Saturday": 22,
+    "Sunday": 24,
+}
+# 🚨 Don't change code above 👆
+
+
+# Write your code 👇 below:
+weather_f = {day: round(temp_c * 1.8 + 32, 2) for (day, temp_c) in weather_c.items()}
+
+
+print(weather_f)
